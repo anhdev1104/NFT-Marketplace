4bef66b29a091e461a25a6ea5a0debaf7aa103e0
Anh Bui <anhbhpd08263@fpt.edu.vn>
2023-05-25 00:31:16 +0700

    update source code

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Xử lí header khi sự kiện scroll diễn ra
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('#header__top')
+    const scrolled = window.scrollY
+    
+    if (scrolled > 0) {
+        header.style.backgroundColor = 'rgba(3, 3, 3, 0.94)'
+    } else {
+        header.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    }
+})
