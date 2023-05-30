38250de187c9582d4b1fb1ff01801ad533cad239
Anh Bui <anhbhpd08263@fpt.edu.vn>
2023-05-30 12:30:46 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,33 @@ window.addEventListener('scroll', () => {
         header.style.backgroundColor = 'rgba(0, 0, 0, 0)'
     }
 })
+
+// Tab UI nav header
+const tabs = document.querySelectorAll('.header__menu-item')
+const line = document.querySelector('.line')
+const logo = document.querySelector('.header__logo')
+const tabActive = document.querySelector('.header__menu-item.invalid-nav')
+
+requestIdleCallback(function () {
+    line.style.left = tabActive.offsetLeft + 'px'
+    line.style.width = tabActive.offsetWidth + 'px'
+})
+
+logo.onclick = function () {
+    line.style.left = tabActive.offsetLeft + 'px'
+    line.style.width = tabActive.offsetWidth + 'px'
+    tabs[0].classList.add('active')
+}
+
+tabs.forEach((tab) => {
+    tab.onclick = function() {
+        line.style.left = this.offsetLeft + 'px'
+        line.style.width = this.offsetWidth + 'px'
+
+        this.classList.add('invalid-nav')
+    }
+
+
+})
+
+
